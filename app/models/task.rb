904e296fc65e80d79c5c68a5818596bbde16be68
jgarciach <jgarciach@gmail.com>
2012-12-02 13:49:27 -0500

    Added urgent scope to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ActiveRecord::Base
   attr_accessible :description, :reward, :title, :urgent, :user_id
   validates :title, :reward, :description, presence: true
+  scope :urgent, where(urgent: true)
 end
