c95d89e4915876dceeff65e04285fd9102a84e12
jgarciach <jgarciach@gmail.com>
2012-12-02 13:51:04 -0500

    Added not urgent scope to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ActiveRecord::Base
   attr_accessible :description, :reward, :title, :urgent, :user_id
   validates :title, :reward, :description, presence: true
   scope :urgent, where(urgent: true)
+  scope :not_urgent, where(urgent: false)
 end
