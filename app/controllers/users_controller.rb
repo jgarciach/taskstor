dc85a563ff849819e2d81a0f293712717c855d85
jgarciach <jgarciach@gmail.com>
2012-12-02 00:21:31 -0500

    Only user can edit its own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   # GET /users
   # GET /users.json
   def index
@@ -15,9 +16,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
+    if is_user?(@user)
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @user }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to :back, notice: "Not You!" }
+      end
     end
   end
 
@@ -35,6 +42,9 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
+    if !is_user?(@user)
+      format.html { redirect_to :back, notice: 'Not you!' }
+    end
   end
 
   # POST /users
@@ -73,11 +83,20 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+    if !is_user?(@user)
+      format.html { redirect_to @user, notice: 'Not you!' }
+    else
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url }
+        format.json { head :no_content }
+      end
     end
+  end
+
+  private
+  def is_user? user
+    user == current_user
   end
 end
