ea0d9295800315a969d9a62417ad3bd77d543a6b
jgarciach <jgarciach@gmail.com>
2012-12-03 00:42:26 -0500

    Removed unnecesary status validator

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,17 +1,7 @@
-class StatusValidator < ActiveModel::Validator
-  def validate(record)
-    if record.status != "unclaimed" || record.status != "in_progess" || record.status != "completed"
-      record.errors[:status] << "Status not valid. "
-    end
-  end
-end
-
 class Task < ActiveRecord::Base
   attr_accessible :description, :reward, :runner_id, :status, :title, :urgent, :user_id
 
-  include ActiveModel::Validations
   validates :title, :reward, :description, presence: true
-  validates_with StatusValidator 
   
   scope :urgent, where(urgent: true)
   scope :not_urgent, where(urgent: false)
