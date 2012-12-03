fed1e5390cf1955e3b54776c774f8287b833dbdb
jgarciach <jgarciach@gmail.com>
2012-12-03 03:01:20 -0500

    Modified model method

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,13 +9,13 @@ class Task < ActiveRecord::Base
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   belongs_to :runner, class_name: "User", foreign_key: :runner_id
 
-  def accept(runner)
+  def self.accept(runner)
     self.runner_id = runner.id
     self.status = "in_progress"
     self.save
   end
 
-  def complete
+  def self.complete
     self.status = "completed"
     self.save
   end
